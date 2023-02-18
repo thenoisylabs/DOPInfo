8357d8e5334e7328dfc4a86d095a6470d59fd114
ShriharshPathak <shriharshpathak@gmail.com>
2023-02-18 22:28:09 +0530

    Initial Commit

---
+++ app/src/main/java/com/noisyLabs/dopinfo/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.noisyLabs.dopinfo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.noisyLabs.dopinfo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val view = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(view.root)
+    }
+}
